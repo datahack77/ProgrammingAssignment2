9aa1df402c4482ff9eeade0821b388ceb454646c
Trey <trey@trey-NV53A.(none)>
2014-08-18 16:19:38 -0400

    First attempt at implementing functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,19 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+##The R functions below can be used to cache inverses of matrices and avoid unnessary
+##time-consuming computations.  The cache mechanism is implemented by taking advantage
+##of R's scoping rules and the <<- operator that allows variable assignments to be made
+##outside of a function's environment and inside the function's parent (enclosing)
+##environment.  Note that these functions assume that the input matirx m is invertible!
 
-## Write a short comment describing this function
 
+#This function creates a special "matrix" object that can cache its inverse.
 makeCacheMatrix <- function(x = matrix()) {
 
+     inv <- NULL     
+     set <- function(y) { #make assignments in the makeCacheMatrix environment. 
+          x <<- y  
+          inv <<- NULL
+     }     
+     get <- function() x
+     setInv <- function(t) inv <<- t #make assignment in the makeCacheMatrix environment
+     getInv <- function() inv
+     list(set = set, get = get,
+          setInv = setInv,
+          getInv = getInv)
      
      
 }#end function makeCacheMatrix
 
 
-## Write a short comment describing this function
+#This function computes the inverse of the special "matrix" returned by
+#makeCacheMatrix above. If the inverse has already been calculated 
+#(and the matrix has not changed), then the cachesolve will retrieve
+#the inverse from the cache.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve <- function(x, ...) {      
 
+     inv <- x$getInv()
+     if(!is.null(inv)) {
+          message("getting cached data")
+          return(inv)
+     }
+     data <- x$get()
+     inv <- solve(data, ...) #compute the matrix inverse
+     x$setInv(inv)
+     inv   # Return a matrix that is the inverse of 'x'
+     
 }#end function cacheSolve
 
